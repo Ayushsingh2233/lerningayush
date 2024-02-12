1ddec20822b46aaaa3af4123b51347dd0d79e2b2
Ayushsingh2233 <ayushabhi02002@gmail.com>
2024-02-12 16:04:57 +0530

    modified:   public/index.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,145 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Perentf from "./FuntionComponent/LiftingStateUp/Perentf";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Perentf />
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Perent from "./Classcomponent/LifitingStateupExample/Perent";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(
+//     <>
+//         <Perent/>
+//     </>
+// )
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import StateManageExample from "./Classcomponent/StateManageExample";
+// import StateManageExampleF from "./FuntionComponent/StateManagementExampleF";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(
+//     <>
+//         <StateManageExample/>
+//         <hr/>
+//         <StateManageExampleF/>
+//     </>
+// )
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import ImportExportF,{name1,arry1,emp1,display1} from "./FuntionComponent/ImportExportF";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(
+//     <>
+//         <ImportExportF/>
+//         <h3>Name : {name1}</h3>
+//         <h3>Array : {arry1.join(" ")} </h3>
+//         <table border="1px" cellPadding="10px">
+//             <tbody>
+//             <tr>
+//                 <th>ID</th>
+//                 <td>{emp1.id}</td>
+//             </tr>
+//             <tr>
+//                 <th>Name</th>
+//                 <td>{emp1.name}</td>
+//             </tr>
+//             <tr>
+//                 <th>Degination</th>
+//                 <td>{emp1.deg}</td>
+//             </tr>
+//             </tbody>
+//         </table>
+//         {display1()}
+//     </>
+// )
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import ImportExportExample,{name1,arry1,emp1,display1} from "./Classcomponent/ImportExportExample";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(
+//     <>
+//         <ImportExportExample/>
+//         <h3>Name : {name1}</h3>
+//         <h3>Array : {arry1.join(" ")} </h3>
+//         <table border="1px" cellPadding="10px">
+//             <tbody>
+//             <tr>
+//                 <th>ID</th>
+//                 <td>{emp1.id}</td>
+//             </tr>
+//             <tr>
+//                 <th>Name</th>
+//                 <td>{emp1.name}</td>
+//             </tr>
+//             <tr>
+//                 <th>Degination</th>
+//                 <td>{emp1.deg}</td>
+//             </tr>
+//             </tbody>
+//         </table>
+//         {display1()}
+//     </>
+// )
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Sample from "./Classcomponent/Sample";
+// import SampleF from "./FuntionComponent/SampleF";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(
+//     <>
+//         <Sample/>
+//         <hr/>
+//         <SampleF/>
+//     </>
+// )
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(
+//     <>
+//         <h1>hello world</h1>
+//         <h1>ayush singh</h1>
+//         <hr/>
+//         <ol>
+//             <h1>list..</h1>
+//             <li>javascript</li>
+//             <li>react</li>
+//             <li>nodejs</li>
+//             <li>mongodb</li>
+//         </ol>
+//         <table>
+//             <tbody>
+//                 <tr>
+//                     <th>name</th>
+//                     <th>age</th>
+//                     <th>salary</th>
+//                 </tr>
+//                 <tr>
+//                     <td>Ayush</td>
+//                     <td>21</td>
+//                     <td>9500</td>
+//                 </tr>
+//             </tbody>
+//         </table>
+//     </>
+// )
